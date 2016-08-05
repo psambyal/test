d5fe3a5692b6afb0609fbb5e2f35396cd637e4b9
Pritika SAMBYAL <pritika.sambyal@amadeus.com>
2016-08-05 12:08:40 +0530

    project added

---
+++ src/test/java/com/amadeus/AppTest.java
@@ -0,0 +1,17 @@
+package com.amadeus;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.amadeus.App;
+
+public class AppTest {
+
+	@Test
+	public void testLengthOfTheUniqueKey() {
+
+		App obj = new App();
+		Assert.assertEquals(36, obj.generateUniqueKey().length());
+
+	}
+}
